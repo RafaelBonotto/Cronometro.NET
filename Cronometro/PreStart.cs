e5c33e214966ad24ad73842f3eab963f2c362df0
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-04-13 14:38:45 -0300

    Update PreStart.cs

--- Cronometro/PreStart.cs
+++ Cronometro/PreStart.cs
@@ -1,7 +1,21 @@
-﻿namespace Cronometro
+﻿using System;
+using System.Threading;
+
+namespace Cronometro
 {
     public class PreStart
     {
-
+        public void PreInicio(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(2500);
+            Start start = new();
+            start.Iniciar(time);
+        }
     }
 }
