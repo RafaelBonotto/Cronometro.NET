f55220de841c5c3fd028b3b38363d9a24ef9552f
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-04-13 14:40:43 -0300

    Update Tools.cs

--- Cronometro/Tools.cs
+++ Cronometro/Tools.cs
@@ -1,10 +1,29 @@
-﻿namespace Cronometro
+﻿using System;
+
+namespace Cronometro
 {
     public class Tools
     {
         public void Menu()
         {
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
 
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if (type == 'm')
+                multiplier = 60;
+
+            if (time == 0)
+                System.Environment.Exit(0);
+            PreStart preStart = new();
+            preStart.PreInicio(time * multiplier);
         }
     }
 }
