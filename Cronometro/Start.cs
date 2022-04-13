9ebe5646bcb804d37e96df006a70573958697495
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-04-13 14:38:49 -0300

    Update Start.cs

--- Cronometro/Start.cs
+++ Cronometro/Start.cs
@@ -1,7 +1,25 @@
-﻿namespace Cronometro
+﻿using System;
+using System.Threading;
+
+namespace Cronometro
 {
     public class Start
     {
-
+        public void Iniciar(int tempo)
+        {
+            int currentTime = 0;
+            while (currentTime != tempo)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado");
+            Thread.Sleep(2500);
+            Tools tools = new();
+            tools.Menu();
+        }
     }
 }
